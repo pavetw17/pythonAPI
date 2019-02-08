92bafdf50ed7cde2bbadc232a5234341b1ed55b5
pavetw17 <yhw0130@autuni.ac.nz>
2019-02-08 22:18:59 +1300

    name object

--- restful.py
+++ restful.py
@@ -1,9 +1,15 @@
 from flask import Flask, jsonify, request
 app = Flask(__name__) #define app using FLask
 
+languages = [{'name' : 'Javascript'}, {'name':'Python'}, {'name':'Ruby'}]
+
 @app.route('/', methods=['GET'])
 def test():
     return jsonify({'message':'It works!'})
+
+@app.route('/lang', methods=['GET'])
+def returnAll():
+    return jsonify({'languages': languages})
 
 if __name__ == '__main__':
     app.run(debug=True,port=8080)
