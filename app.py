1ef47357401fa2e2ebcd75d2cdc5a17a11f7f350
pavetw17 <yhw0130@autuni.ac.nz>
2019-02-10 00:07:06 +1300

    Login page

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, redirect, url_for, request
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return redirect('/login')
+
+@app.route('/home')
+def home():
+    return 'Login success!'
+
+#Route for handling the login page
+@app.route('/login',methods=['GET','POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=8080, debug=True)
